d7c7e78072c347d378fc6de5df2fd041a28586fd
a <justinc1127@nycstudents.net>
2025-10-23 10:04:36 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,32 @@
-microcenter{
-    {    item=
-        "name":"Nvidia GeForce 5070 "
-        "price":549.99
-        "description": 
-        }
-}
+# microcenter{
+#     {    item=
+#         "name":"Nvidia GeForce 5070 "
+#         "price":549.99
+#         "description": 
+#         }
+# }
+
+
+
+count=0
+quarter=input()
+first=input()
+second=input()
+third=input()
+while quarter>0:
+    quarter-1
+    count+=1
+    first+=1
+    if first==35:
+        quarter+30
+        first=0
+    quarter-1
+    count+=1
+    if second==100:
+        quarter+60
+        second=0
+    quarter-1
+    count+=1
+    if third==10:
+        quarter+9
+    print(f"She plays{count}times before poor")
