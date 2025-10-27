bdaa84cc322f530269b5f74f3fafd9bbf137e74c
a <justinc1127@nycstudents.net>
2025-10-27 10:14:31 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
-microcenter={
-    item={
-        "name":"Nvidia GeForce 5070 ",
-        "price":549.99,
-        "description":
-    }
-}
+
+# item= {
+#     "name":"Nvidia GeForce 5070 ",
+#     "price":549.99,
+#     "description":"12GB GDDR7 192-bit Memory"
+
+# }
 
 
-item = {
+x = {
     "name": "Samsung 55\" 4K UHD TV",
     "price": 429.99,
     "department": "Televisions",
     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
 }
+
