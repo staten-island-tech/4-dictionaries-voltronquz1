62bfce41c77d8dad6a60b7f285ab0fed4b048b37
a <justinc1127@nycstudents.net>
2025-10-30 10:16:23 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,37 +1,38 @@
 
 cart=[]
 cost=0
-store=[ 
+store = [{
 {
-        "name":"gum",
-        "price":3.99,
-        "description":"chewing gum"
-},
+    "name" : "watermelon gum",
+    "price" : 3.99,
+    "description:": "chewing gum"
+  },
 {
-    "name":"Chips",
-    "price":"1.00",
+    "name" : "lays chips",
+    "price" : 1.00,
     "description":"salted chips"
- }, 
+  },
 {
-    "name":"water ",
-    "price":"1.00",
+    "name" : "Poland Spring",
+    "price" : 1.00,
     "description":"Get hydrated"
-},
+  },
 {
-    "name":"coffee",
-    "price":"2.50",
-    "description":"get energy and caffeined"
-},
-]
+      "name":"instant coffee",
+      "price":2.50,
+      "description":"get caffeined"
+  }
+}]
 
 item=input("What item do you want: gum, Chips,water (type done when done)")
 
 while item != "done":
-    if "gum" or "coffee" or "chips" or "water":
+    if store["name"]:
         print(input(f"Are you sure you want {item}"))
         if "Yes":
-            cost+=3.99
+            for index,item in enumerate(store):
+                cost+=store["price"]
+
             cart.append(item)
             print(f"You have {cart} and the cost is {cost}")
             input("What else do you want")
-    
