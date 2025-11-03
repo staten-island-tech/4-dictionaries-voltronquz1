0fee88b5533aa735814d2c1d6478e40a853d7b0d
a <justinc1127@nycstudents.net>
2025-11-03 10:17:18 -0500

    Update test.py

--- test.py
+++ test.py
@@ -25,46 +25,18 @@ store = [
     }
 
 ]
-""" for index, item in enumerate(store):
-    print(index, ":", item["name"], item["price"]) """
-item=input("What item do you want:1. gum, 2.Chips,3.water ")
-while True:
-        print(input(f"Are you sure you want {item}"))
-        if "Yes" or "yes":
-            for index,item in enumerate(store):
-                cost+=store["price"]
-            cart.append(item)
-            print(f"You have {cart} and the cost is {cost}")
-            input("What else do you want")
-        if "no" or "No":
-            break   
+buying=True
+while buying:
+        print("What item do you want:1. gum, 2.Chips,3.water ")
+        item=int(input("1,2,3 "))
+        item-=1 #list reads from 0,1,2,3..etc so if 1 it gives correct input
+        result=store[item]
+        cart.append(result)
+        cost+=result["price"]
+        print(input("Do you want to continue?Y/N"))
+        if "Y" or "y":
+              buying=True
 
-
-
-# print(f"You have these items:{cart} and the cost {cost}")
-# for index, item in enumerate(items):
-#         print(index, ":" ,(item)["name"])
-#         items[1]["name"]
-
-
-
-# cart = []
-# cost = 0
-# choice = int(input("What would you like to buy?"))
-# cart.append(items[choice])
-# cost += items[choice]["price"]
-# print(f"Added {items[choice]['name']} into cart")
-
-# while True:
-#     checkout = (input("Would you like to continue shopping? (Yes/No)"))
-#     if checkout == "Yes":
-#         choice = int(input("What else would you like to buy?"))
-#         cart.append(items[choice])
-#         cost += items[choice]["price"]
-#         print(f"Added {items[choice]['name']} into cart")
-#     if checkout == "No":
-#          break
-
-# for item in cart: 
-#     print(f'{item["name"]} ${int(item["price"])}')
-# print(f"Total: ${cost}")
+        elif "N" or "n":
+            break
+print(cart)
